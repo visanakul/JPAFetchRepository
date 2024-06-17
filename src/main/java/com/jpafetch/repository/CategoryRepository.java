665885ff88feaca8082f4c66df7e21f47cb0ebd0
vishal <visanakul@gmail.com>
2024-06-17 11:37:11 +0530

    CategoryRepository '@EntityGraph Added'. Report file 13 added for result

--- src/main/java/com/jpafetch/repository/CategoryRepository.java
+++ src/main/java/com/jpafetch/repository/CategoryRepository.java
@@ -1,11 +1,21 @@
 package com.jpafetch.repository;
 
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jpafetch.entity.CategoryEntity;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, UUID>{
-	
+	@EntityGraph(
+		    type = EntityGraphType.FETCH,
+		    attributePaths = {
+		      "productEntities",
+		      "productEntities.categoryEntity"
+		    }
+		  )
+	List<CategoryEntity> findAll();
 }
